60b10512129a77ba384e382853f3b61042bf00a6
Juvaly <juvaly@gmail.com>
2013-12-19 18:15:29 +0200

    validation when creating a professional profile

--- classifieds/DTO/Request/UpdateProfile.cs
+++ classifieds/DTO/Request/UpdateProfile.cs
@@ -11,5 +11,25 @@ namespace Classy.Models.Request
         public string ProfileId { get; set; }
         public IList<CustomAttribute> Metadata { get; set; }
         public Seller SellerInfo { get; set; }
+        public string UpdateType { get; set; }
+    }
+
+    public class UpdateProfileValidator : AbstractValidator<UpdateProfile>
+    {
+        
+        public UpdateProfileValidator()
+        {
+            When(x => x.UpdateType == "CreateProfessionalProfile", () =>
+            {
+                RuleFor(x => x.SellerInfo.DisplayName).NotEmpty();
+                RuleFor(x => x.SellerInfo.ContactInfo.FirstName).NotEmpty();
+                RuleFor(x => x.SellerInfo.ContactInfo.LastName).NotEmpty();
+                RuleFor(x => x.SellerInfo.ContactInfo.Phone).NotEmpty();
+                RuleFor(x => x.SellerInfo.Category).NotEmpty().NotEqual("0");
+                RuleFor(x => x.SellerInfo.ContactInfo.Location.Address.Street1).NotEmpty();
+                RuleFor(x => x.SellerInfo.ContactInfo.Location.Address.City).NotEmpty();
+                RuleFor(x => x.SellerInfo.ContactInfo.Location.Address.PostalCode).NotEmpty();
+            }); 
+        }
     }
 }
