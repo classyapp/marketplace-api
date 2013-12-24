43a23afe1bc6719416ee9d439f0c9da5f86d1501
Juvaly <juvaly@gmail.com>
2013-12-24 20:25:49 +0200

    small update of pricing info

--- Classy.Models/PurchaseOption.cs
+++ Classy.Models/PurchaseOption.cs
@@ -5,15 +5,10 @@ using System.Text;
 
 namespace Classy.Models
 {
-    public class Variant
-    {
-        public string Title { get; set; }
-        public string Value { get; set; }
-    }
-
     public class PurchaseOption
     {
-        public IList<Variant> Variants { get; set; }
+        public string VariantKey { get; set; } // Size, Color, Model, etc.
+        public string VariantValue { get; set; } // S, M, L, Red, Blue, Pink/Crimson, etc.
         public string SKU { get; set; }
         public double Price { get; set; }
         public double Quantity { get; set; }
