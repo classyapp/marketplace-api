703b48bd62a000662a5ed135aae879aa8d64800b
Juvaly <juvaly@gmail.com>
2014-02-19 20:54:09 +0200

    removed extra page property in GetListingsByProfileId

--- classifieds/DTO/Request/GetListingsByProfileId.cs
+++ classifieds/DTO/Request/GetListingsByProfileId.cs
@@ -14,7 +14,6 @@ namespace Classy.Models.Request
         public bool IncludeComments { get; set; }
         public bool FormatCommentsAsHtml { get; set; }
         public bool IncludeDrafts { get; set; }
-        public int Page { get; set; }
     }
 
     public class GetListingsByProfileIdValidator : AbstractValidator<GetListingsByProfileId>
