5c30a627a13e58a0cea4fcc111f63ea99ea60ae3
Michael Bar <barmic@gmail.com>
2014-03-07 19:09:04 -0300

    Memory cache for images until uploaded to S3
    Thumbs resize refactor

--- classifieds/Manager/DefaultThumbnailManager.cs
+++ classifieds/Manager/DefaultThumbnailManager.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Web;
@@ -23,42 +25,13 @@ namespace classy.Manager
             try
             {
                 if (height == 0) height = width;
-                MemoryStream memoryStream;
-                Bitmap squareImage;
+                Stream memoryStream;
 
                 Stream originalImage = StorageRepository.GetFile(originKey);
 
-                // Crop to square
-                //using (Image originalImg = Image.FromStream(originalImage))
-                //{
-                //    int minDimension;
-                //    int startX = 0;
-                //    int startY = 0;
-                //    if (originalImg.Height < originalImg.Width)
-                //    {
-                //        minDimension = originalImg.Height;
-                //        startX = (originalImg.Width - originalImg.Height) / 2;
-                //    }
-                //    else
-                //    {
-                //        minDimension = originalImg.Width;
-                //        startY = (originalImg.Height - originalImg.Width) / 2;
-                //    }
-
-                //    using (Bitmap bmp = new Bitmap(originalImg))
-                //    {
-                //        squareImage = bmp.Clone(new Rectangle(startX, startY, minDimension, minDimension), System.Drawing.Imaging.PixelFormat.DontCare);
-                //    }
-                //}
-
-                // Create thumb
-                using (Bitmap bmp = new Bitmap(originalImage))
+                lock (originalImage)
                 {
-                    using (var thumbnail = bmp.GetThumbnailImage(width, height, () => false, System.IntPtr.Zero))
-                    {
-                        memoryStream = new MemoryStream(thumbnail.Size.Height * thumbnail.Size.Width);
-                        thumbnail.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Jpeg);
-                    }
+                    memoryStream = GenerateThumbnail(originalImage, width, height);
                 }
                 return memoryStream;
             }
@@ -67,5 +40,46 @@ namespace classy.Manager
                 throw ex;
             }
         }
+
+        private Stream GenerateThumbnail(Stream originalImage, int width, int height)
+        {
+            MemoryStream memoryStream = null;
+            using (Image source = Image.FromStream(originalImage))
+            {
+                // Resize the original
+                int newWidth = 0;
+                int newHeight = 0;
+
+                if ((float)height / (float)source.Height > (float)width / (float)source.Width)
+                {
+                    newHeight = height;
+                    newWidth = (int)(width * (((float)height / (float)source.Height) / ((float)width / (float)source.Width)));
+                }
+                else
+                {
+                    newHeight = (int)(height * (((float)width / (float)source.Width) / ((float)height / (float)source.Height)));
+                    newWidth = width;
+                }
+
+                using (Bitmap bitmap = new Bitmap(newWidth, newHeight))
+                {
+                    Graphics g = Graphics.FromImage((Image)bitmap);
+                    g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+
+                    g.DrawImage(source, 0, 0, newWidth, newHeight);
+                    g.Dispose();
+
+                    // Crop
+                    using (Bitmap thumbnail = bitmap.Clone(new Rectangle { X = (newWidth - width) / 2, Y = (newHeight - height) / 2, Width = width, Height = height }, PixelFormat.DontCare))
+                    {
+                        memoryStream = new MemoryStream(width * height);
+                        thumbnail.Save(memoryStream, ImageFormat.Jpeg);
+                    }
+                }
+            }
+
+            return memoryStream;
+        }
+
     }
 }
