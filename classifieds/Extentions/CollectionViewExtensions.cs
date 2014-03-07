9b8c10b24f9c120003ea18b9f7eb27000c2b898e
Michael Bar <barmic@gmail.com>
2014-03-07 14:58:06 -0300

    Basic cover photos functionality for projects

--- classifieds/Extentions/CollectionViewExtensions.cs
+++ classifieds/Extentions/CollectionViewExtensions.cs
@@ -15,6 +15,7 @@ namespace classy
             var to = from.TranslateTo<CollectionView>();
             to.IncludedListings = from.IncludedListings.Select(l => new Classy.Models.Response.IncludedListingView { Id = l.Id, Comments = l.Comments, ListingType = l.ListingType, ProfileId = from.ProfileId }).ToList();
             to.Thumbnails = (from.Thumbnails ?? new List<MediaThumbnail>()).Select(t => new MediaThumbnailView { Height = t.Height, Key = t.Key, Width = t.Width, Url = t.Url }).ToList();
+
             return to;
         }
 
