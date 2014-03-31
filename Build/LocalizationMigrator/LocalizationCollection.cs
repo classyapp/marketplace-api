12f5a166ae24d21c9e71663384f9ac4f10c9c80c
Noah Blumenthal <noah@hackerhasid.com>
2014-03-30 22:11:46 -0400

    added localization migrator

---
+++ Build/LocalizationMigrator/LocalizationCollection.cs
@@ -0,0 +1,126 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YamlDotNet.RepresentationModel;
+
+namespace LocalizationMigrator
+{
+    public class LocalizationCollection
+    {
+        private MongoDatabase _db;
+        private MongoCollection<LocalizationMigration> _migrationCollection;
+        private MongoCollection<LocalizationResource> _resourceCollection;
+        private MongoCollection<LocalizationListResource> _resourceListCollection;
+        private Dictionary<string, bool> _currentMigrations;
+        public LocalizationCollection(MongoDatabase db)
+        {
+            _migrationCollection = db.GetCollection<LocalizationMigration>("LocalizationMigrations");
+            _resourceCollection = db.GetCollection<LocalizationResource>("resources");
+            _resourceListCollection = db.GetCollection<LocalizationListResource>("listresourcess");
+            loadCurrentMigrations();
+        }
+
+        public void Migrate(string path)
+        {
+            Migrate(new DirectoryInfo(path));
+        }
+        public void Migrate(DirectoryInfo migrationDirectory)
+        {
+            var migrations = migrationDirectory.GetFiles("*.yml").Where(x => !_currentMigrations.ContainsKey(x.Name));
+            foreach (var file in migrations.OrderBy(x => x.Name))
+            {
+                var version = file.Name;
+                var contents = File.ReadAllText(file.FullName);
+                saveResourcesFromYaml(contents);
+                _migrationCollection.Insert(new LocalizationMigration()
+                {
+                    Version = version
+                });
+            }
+        }
+
+        private void saveResourcesFromYaml(string yamlContents)
+        {
+            using (var reader = new StringReader(yamlContents))
+            {
+                var yaml = new YamlStream();
+                yaml.Load(reader);
+
+                var mapping = (YamlMappingNode)yaml.Documents[0].RootNode;
+                foreach (var element in mapping.Children) // e.g. "window"
+                {
+                    var word = ((YamlScalarNode)element.Key).Value;
+                    if (element.Value is YamlSequenceNode)
+                    {
+                        saveResourceList(word, (YamlSequenceNode)element.Value);
+                    }
+                    else
+                    {
+                        saveResource(word, (YamlMappingNode)element.Value);
+                    }
+                }
+            }
+        }
+
+        private void saveResourceList(string word, YamlSequenceNode listItems)
+        {
+            var resourceListDoc = new LocalizationListResource()
+            {
+                Key = word,
+                AppId = "v1.0",
+                ListItems = new List<ListItem>() { }
+            };
+
+            foreach (var node in listItems.Children)
+            {
+                var wrap = (YamlMappingNode)node;
+                var listItem = new ListItem() {
+                    Value = ((YamlScalarNode)wrap.Children.First().Key).Value,
+                    Text = new Dictionary<string, string>()
+                };
+                foreach(var child in wrap.Children.Skip(1))
+                {
+                    var resourceName = ((YamlScalarNode)child.Key).Value;
+                    var resourceValue = ((YamlScalarNode)child.Value).Value;
+                    listItem.Text.Add(resourceName, resourceValue);
+                }
+                resourceListDoc.ListItems.Add(listItem);
+            }
+
+            _resourceListCollection.Insert(resourceListDoc);
+        }
+        private void saveResource(string word, YamlMappingNode languages)
+        {
+            var resourceDoc = new LocalizationResource()
+            {
+                Key = word,
+                AppId = "v1.0",
+                Values = new Dictionary<string, string>()
+            };
+            foreach (var resource in languages)
+            {
+                var resourceName = ((YamlScalarNode)resource.Key).Value;
+                var resourceValue = ((YamlScalarNode)resource.Value).Value;
+                resourceDoc.Values.Add(resourceName, resourceValue);
+            }
+            _resourceCollection.Insert(resourceDoc);
+        }
+
+
+        private void loadCurrentMigrations()
+        {
+            _currentMigrations = new Dictionary<string, bool>();
+            foreach (LocalizationMigration migration in _migrationCollection.FindAll())
+            {
+                var version = migration.Version;
+                _currentMigrations[version] = true;
+            }
+        }
+    }
+}
---
+++ Build/LocalizationMigrator/LocalizationResource.cs
@@ -0,0 +1,69 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.IdGenerators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalizationMigrator
+{
+    /// <summary>
+    /// the base class for all model objects
+    /// </summary>
+    public class BaseObject
+    {
+        /// <summary>
+        /// the database id of the object
+        /// </summary>
+        [BsonId(IdGenerator = typeof(StringObjectIdGenerator))]
+        public string Id { get; set; }
+        /// <summary>
+        /// the id of the application the object belongs to
+        /// </summary>
+        public string AppId { get; set; }
+        /// <summary>
+        /// a timestamp of the creation of the object. defaults to UtcNow
+        /// </summary>
+        public DateTime Created { get; set; }
+
+        public BaseObject()
+        {
+            Created = DateTime.UtcNow;
+        }
+    }
+
+    public class LocalizationResource : BaseObject
+    {
+        /// <summary>
+        /// the resoruce key.
+        /// </summary>
+        public string Key { get; set; }
+        /// <summary>
+        /// dictionary of values. the key for the dictionary is the culture name.
+        /// </summary>
+        public IDictionary<string, string> Values { get; set; }
+        public string Description { get; set; }
+        public string ContextScreenshotUrl { get; set; }
+        public string ContextUrl { get; set; }
+    }
+
+    public class ListItem
+    {
+        public string Value { get; set; }
+        public string ParentValue { get; set; }
+        public IDictionary<string, string> Text { get; set; }
+    }
+
+    public class LocalizationListResource : BaseObject
+    {
+        /// <summary>
+        /// the resoruce key.
+        /// </summary>
+        public string Key { get; set; }
+        /// <summary>
+        /// dictionary of values. the key for the dictionary is the culture name.
+        /// </summary>
+        public IList<ListItem> ListItems { get; set; }
+    }
+}
