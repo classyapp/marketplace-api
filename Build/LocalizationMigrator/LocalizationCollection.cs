0967d2db327c719076b86d27def83d78ca9510cc
Noah Blumenthal <noah@hackerhasid.com>
2014-04-01 17:03:11 -0400

    updated localization migrator to use json and take app_id into account

--- Build/LocalizationMigrator/LocalizationCollection.cs
+++ Build/LocalizationMigrator/LocalizationCollection.cs
@@ -1,13 +1,13 @@
-﻿using MongoDB.Bson;
+﻿using Classy.Models;
+using MongoDB.Bson;
 using MongoDB.Driver;
-using Newtonsoft.Json.Linq;
+using ServiceStack.Text;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using YamlDotNet.RepresentationModel;
 
 namespace LocalizationMigrator
 {
@@ -32,12 +32,12 @@ namespace LocalizationMigrator
         }
         public void Migrate(DirectoryInfo migrationDirectory)
         {
-            var migrations = migrationDirectory.GetFiles("*.yml").Where(x => !_currentMigrations.ContainsKey(x.Name));
+            var migrations = migrationDirectory.GetFiles("*.json").Where(x => !_currentMigrations.ContainsKey(x.Name));
             foreach (var file in migrations.OrderBy(x => x.Name))
             {
                 var version = file.Name;
                 var contents = File.ReadAllText(file.FullName);
-                saveResourcesFromYaml(contents);
+                saveResourcesFromJson(contents);
                 _migrationCollection.Insert(new LocalizationMigration()
                 {
                     Version = version
@@ -45,73 +45,41 @@ namespace LocalizationMigrator
             }
         }
 
-        private void saveResourcesFromYaml(string yamlContents)
+        private void saveResourcesFromJson(string jsonString)
         {
-            using (var reader = new StringReader(yamlContents))
+            var resources = JsonObject.Parse(jsonString);
+            foreach (var word in resources.Keys)
             {
-                var yaml = new YamlStream();
-                yaml.Load(reader);
-
-                var mapping = (YamlMappingNode)yaml.Documents[0].RootNode;
-                foreach (var element in mapping.Children) // e.g. "window"
+                var props = JsonObject.Parse(resources.Child(word));
+                var appId = props.Child("appId");
+                var description = props.Child("description");
+                if (props.Keys.Contains("items", StringComparer.CurrentCultureIgnoreCase))
                 {
-                    var word = ((YamlScalarNode)element.Key).Value;
-                    if (element.Value is YamlSequenceNode)
+                    var resourceListDoc = new LocalizationListResource()
                     {
-                        saveResourceList(word, (YamlSequenceNode)element.Value);
-                    }
-                    else
+                        AppId = appId,
+                        Key = word,
+                        ListItems = new List<ListItem>()
+                    };
+                    var items = JsonSerializer.DeserializeFromString<List<string>>(props.Child("items"));
+                    foreach (var i in items)
                     {
-                        saveResource(word, (YamlMappingNode)element.Value);
+                        resourceListDoc.ListItems.Add(new ListItem() { Value = i });
                     }
+                    _resourceListCollection.Insert(resourceListDoc);
+                }
+                else
+                {
+                    var resourceDoc = new LocalizationResource()
+                    {
+                        AppId = appId,
+                        Key = word,
+                        Description = description
+                    };
+                    _resourceCollection.Insert(resourceDoc);
                 }
             }
         }
-
-        private void saveResourceList(string word, YamlSequenceNode listItems)
-        {
-            var resourceListDoc = new LocalizationListResource()
-            {
-                Key = word,
-                AppId = "v1.0",
-                ListItems = new List<ListItem>() { }
-            };
-
-            foreach (var node in listItems.Children)
-            {
-                var wrap = (YamlMappingNode)node;
-                var listItem = new ListItem() {
-                    Value = ((YamlScalarNode)wrap.Children.First().Key).Value,
-                    Text = new Dictionary<string, string>()
-                };
-                foreach(var child in wrap.Children.Skip(1))
-                {
-                    var resourceName = ((YamlScalarNode)child.Key).Value;
-                    var resourceValue = ((YamlScalarNode)child.Value).Value;
-                    listItem.Text.Add(resourceName, resourceValue);
-                }
-                resourceListDoc.ListItems.Add(listItem);
-            }
-
-            _resourceListCollection.Insert(resourceListDoc);
-        }
-        private void saveResource(string word, YamlMappingNode languages)
-        {
-            var resourceDoc = new LocalizationResource()
-            {
-                Key = word,
-                AppId = "v1.0",
-                Values = new Dictionary<string, string>()
-            };
-            foreach (var resource in languages)
-            {
-                var resourceName = ((YamlScalarNode)resource.Key).Value;
-                var resourceValue = ((YamlScalarNode)resource.Value).Value;
-                resourceDoc.Values.Add(resourceName, resourceValue);
-            }
-            _resourceCollection.Insert(resourceDoc);
-        }
-
 
         private void loadCurrentMigrations()
         {
