86176c674c67399832e5a4c858297d43ec979443
Juvaly <juvaly@gmail.com>
2014-04-02 14:28:40 +0300

    script changes

---
+++ db-scripts/remove-thumbnails-from-external-media.js
@@ -0,0 +1,16 @@
+db.classifieds.find({"ExternalMedia.Thumbnails":{$exists:true}})
+.forEach( function(doc) {
+  var arr = doc.ExternalMedia;
+  var length = arr.length;
+  for (var i = 0; i < length; i++) {
+    delete arr[i]["Thumbnails"];
+  }
+  db.classifieds.save(doc);
+});
+
+db.profiles.update({"Avatar.Thumbnails":{$exists:true}},
+{$unset:{"Avatar.Thumbnails":1}}, {multi:true});
+
+db.collections.update({"Thumbnails":{$exists:true}},
+{$unset:{"Thumbnails":1}}, {multi:true});
+
