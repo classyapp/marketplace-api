641953ade2054904be3f3e7abc1ca287fb40a6e5
Noah Blumenthal <noah@hackerhasid.com>
2014-04-03 13:56:35 -0400

    added route to get all untranslated resources for culture

--- classifieds/Manager/DefaultLocalizationManager.cs
+++ classifieds/Manager/DefaultLocalizationManager.cs
@@ -18,6 +18,17 @@ namespace classy.Manager
             LocalizationRepository = localizationRepository;
         }
 
+        public IEnumerable<LocalizationResourceView> GetAllResources(string appId)
+        {
+            return LocalizationRepository.GetResourcesForApp(appId).Select(x => new LocalizationResourceView()
+            {
+                Key = x.Key,
+                Values = x.Values,
+                Description = x.Description
+            });
+
+        }
+
         public LocalizationResourceView GetResourceByKey(string appId, string key, bool processMarkdown = true)
         {
             var resource = LocalizationRepository.GetResourceByKey(appId, key);
