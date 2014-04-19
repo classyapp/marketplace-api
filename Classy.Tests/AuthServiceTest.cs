1d73d6da8595d7be3814bb621fc3776bc2b41b3f
mfractal <mfractal@gmail.com>
2014-04-19 13:46:09 -0300

    Initial test project setup

---
+++ Classy.Tests/AuthServiceTest.cs
@@ -0,0 +1,54 @@
+﻿using Classy.Auth;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ServiceStack.ServiceClient.Web;
+using ServiceStack.ServiceInterface.Testing;
+using ServiceStack.WebHost.Endpoints;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classy.Tests
+{
+    [TestClass]
+    public class AuthServiceTest
+    {
+        private AppHostHttpListenerBase appHost;
+        private JsonServiceClient client;
+
+        [TestInitialize]
+        public void TestStart()
+        {
+            appHost = new TestAppHost();
+            appHost.Init();
+            appHost.Start("http://localhost:8080/api/");
+
+            client = new JsonServiceClient("http://localhost:8080/api");
+        }
+
+        [TestMethod]
+        public void TestLogin()
+        {
+            try
+            {
+                client.Headers.Add("X-Classy-Env", new { AppId = "v1.0", CultureCode = "he", CountryCode = "IL", CurrencyCode = "ILS" }.ToJson());
+                var response = client.Post<AuthResponse>("/auth",
+                    new Auth.Auth { UserName = "juvaly1", Password = "333444" });
+                Assert.AreEqual(response.UserName, "test1");
+            }
+            catch (WebServiceException ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+
+        [TestCleanup]
+        public void TestEnd()
+        {
+            appHost.Dispose();
+        }
+
+
+    }
+}
