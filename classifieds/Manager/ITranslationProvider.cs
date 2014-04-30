66ec080591b49b5d5161f55e0eeedcf16f9c489f
Juvaly <juvaly@gmail.com>
2014-04-30 20:58:01 +0300

    added new ITranslationProvider

---
+++ classifieds/Manager/ITranslationProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classy.Manager
+{
+    public interface ITranslationProvider
+    {
+        string DetectLanguage(string textToTranslate);
+        IDictionary<string, string> Translate(string textToTranslate, string[] cultures);
+    }
+}
