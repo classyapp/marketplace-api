ec0dcc63a0689343a371b958fb642446356dc2c7
Gilly Barr <gillyb@gmail.com>
2014-05-11 17:03:52 +0300

    initial commit for Search and UtilRunner
    Added a SearchFactory to get an elastic client instance
    added a new project for creating utilities

---
+++ Classy.Interfaces/Search/ListingSearchProvider.cs
@@ -0,0 +1,25 @@
+﻿using Classy.Models.Search;
+using Nest;
+
+namespace Classy.Interfaces.Search
+{
+    public interface IListingSearchProvider
+    {
+        void Index(ListingIndexDto[] listingDtos);
+    }
+
+    public class ListingSearchProvider : IListingSearchProvider
+    {
+        private readonly IElasticClient _client;
+
+        public ListingSearchProvider(ISearchClientFactory searchClientFactory)
+        {
+            _client = searchClientFactory.GetClient("listings");
+        }
+
+        public void Index(ListingIndexDto[] listingDtos)
+        {
+            _client.IndexMany(listingDtos);
+        }
+    }
+}
---
+++ Classy.UtilRunner/Utilities/ListingIndexer.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Classy.Interfaces.Search;
+using Classy.Models;
+using Classy.Models.Search;
+using Funq;
+using MongoDB.Driver;
+using Nest;
+
+namespace Classy.UtilRunner.Utilities
+{
+    public class ListingIndexer : IUtility
+    {
+        private readonly MongoCollection<Listing> _listings;
+        private readonly ISearchClientFactory _searchClientFactory;
+
+        public ListingIndexer(Container container)
+        {
+            _searchClientFactory = container.Resolve<ISearchClientFactory>();
+            var mongoDatabase = container.Resolve<MongoDatabase>();
+            _listings = mongoDatabase.GetCollection<Listing>("classifieds");
+        }
+        
+        public StatusCode Run(string[] args)
+        {
+            var client = _searchClientFactory.GetClient("listings");
+            client.DeleteIndex<ListingIndexDto>();
+            client.CreateIndex("listings", new IndexSettings());
+            client.MapFromAttributes<ListingIndexDto>();
+
+            client = _searchClientFactory.GetClient("listings");
+
+            var toIndex = new List<ListingIndexDto>();
+
+            var cursor = _listings.FindAll().SetLimit(100);
+            foreach (var listing in cursor)
+            {
+                toIndex.Add(new ListingIndexDto
+                {
+                    AddToCollectionCount = listing.AddToCollectionCount,
+                    BookingCount = listing.BookingCount,
+                    ClickCount = listing.ClickCount,
+                    CommentCount = listing.CommentCount,
+                    Content = listing.Content,
+                    FavoriteCount = listing.FavoriteCount,
+                    FlagCount = listing.FlagCount,
+                    Keywords = listing.SearchableKeywords != null ? listing.SearchableKeywords.ToArray() : new string[0],
+                    ListingType = listing.ListingType,
+                    Metadata = (Dictionary<string, string>)listing.Metadata,
+                    PurchaseCount = listing.PurchaseCount,
+                    Title = listing.Title,
+                    ViewCount = listing.ViewCount
+                });
+            }
+
+            client.IndexMany(toIndex.ToArray());
+
+            return StatusCode.Success;
+        }
+    }
+}
