ffe1e46a557b8d67b3e9a180dc7e8708e253663e
Gilly Barr <gillyb@gmail.com>
2014-05-11 18:41:50 +0300

    adding endpoint for searching

--- Classy.Interfaces/Search/ListingSearchProvider.cs
+++ Classy.Interfaces/Search/ListingSearchProvider.cs
@@ -1,25 +1,11 @@
 ﻿using Classy.Models.Search;
-using Nest;
 
 namespace Classy.Interfaces.Search
 {
     public interface IListingSearchProvider
     {
         void Index(ListingIndexDto[] listingDtos);
-    }
 
-    public class ListingSearchProvider : IListingSearchProvider
-    {
-        private readonly IElasticClient _client;
-
-        public ListingSearchProvider(ISearchClientFactory searchClientFactory)
-        {
-            _client = searchClientFactory.GetClient("listings");
-        }
-
-        public void Index(ListingIndexDto[] listingDtos)
-        {
-            _client.IndexMany(listingDtos);
-        }
+        SearchResults<ListingIndexDto> Search(string query);
     }
 }
