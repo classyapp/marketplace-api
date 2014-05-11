8ab579f5e44faf5f91fcecdb9ad043f78f9b9adb
Gilly Barr <gillyb@gmail.com>
2014-05-11 17:03:52 +0300

    initial commit for Search and UtilRunner
    Added a SearchFactory to get an elastic client instance
    added a new project for creating utilities

---
+++ Classy.Interfaces/Search/SearchClientFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Configuration;
+using Nest;
+
+namespace Classy.Interfaces.Search
+{
+    public interface ISearchClientFactory
+    {
+        ElasticClient GetClient(string indexName);
+    }
+
+    public class SearchClientFactory : ISearchClientFactory
+    {
+        public static string ElasticConnectionString = ConfigurationManager.AppSettings["SEARCHBOX_URL"];
+
+        public ElasticClient GetClient(string indexName)
+        {
+            var settings = new ConnectionSettings(new Uri(ElasticConnectionString));
+
+            if (indexName != null)
+                settings.SetDefaultIndex(indexName);
+
+            return new ElasticClient(settings);
+        }
+    }
+}
