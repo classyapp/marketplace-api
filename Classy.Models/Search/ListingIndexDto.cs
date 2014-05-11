9b44a624f5865ae977df8264df3a1ae1d7c1fef0
Gilly Barr <gillyb@gmail.com>
2014-05-11 17:03:52 +0300

    initial commit for Search and UtilRunner
    Added a SearchFactory to get an elastic client instance
    added a new project for creating utilities

---
+++ Classy.Models/Search/ListingIndexDto.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using Nest;
+
+namespace Classy.Models.Search
+{
+    [ElasticType(Name = "listing")]
+    public class ListingIndexDto
+    {
+        [ElasticProperty(Index = FieldIndexOption.analyzed)]
+        public string Title { get; set; }
+        [ElasticProperty(Index = FieldIndexOption.analyzed)]
+        public string Content { get; set; }
+        public string ListingType { get; set; }
+        public string[] Keywords { get; set; } // comes from SearchableKeywords
+
+        // Scoring
+        public int CommentCount { get; set; }
+        public int FavoriteCount { get; set; }
+        public int FlagCount { get; set; }
+        public int ViewCount { get; set; }
+        public int ClickCount { get; set; }
+        public int PurchaseCount { get; set; }
+        public int BookingCount { get; set; }
+        public int AddToCollectionCount { get; set; }
+
+        // not sure if we need these...
+        //public int DisplayOrder { get; set; }
+        //public ContactInfo ContactInfo { get; set; }
+        //public PricingInfo PricingInfo { get; set; }
+        //public TimeslotSchedule SchedulingTemplate { get; set; }
+
+        [ElasticProperty(Type = FieldType.@object)]
+        public Dictionary<string, string> Metadata { get; set; }
+    }
+}
