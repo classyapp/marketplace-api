0e0727a1128f1670cddf702bf038e7d57364ae6a
Gilly Barr <gillyb@gmail.com>
2014-05-11 18:41:50 +0300

    adding endpoint for searching

---
+++ Classy.Tests/Search/ListingSearchProviderTests.cs
@@ -0,0 +1,15 @@
+﻿using classy.Manager.Search;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Classy.Tests.Search
+{
+    [TestClass]
+    public class ListingSearchProviderTests
+    {
+        [TestMethod]
+        public void Search_WordInTitle_ReturnRelevantDocuments()
+        {
+            
+        }
+    }
+}
---
+++ classifieds/Manager/Search/ListingSearchProvider.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Classy.Interfaces.Search;
+using Classy.Models.Search;
+using Nest;
+
+namespace classy.Manager.Search
+{
+    public class ListingSearchProvider : IListingSearchProvider
+    {
+        private readonly IElasticClient _client;
+
+        public ListingSearchProvider(ISearchClientFactory searchClientFactory)
+        {
+            _client = searchClientFactory.GetClient("listings");
+        }
+
+        public void Index(ListingIndexDto[] listingDtos)
+        {
+            _client.IndexMany(listingDtos);
+        }
+
+        public SearchResults<ListingIndexDto> Search(string query)
+        {
+            var queryDescriptor = new QueryDescriptor<ListingIndexDto>();
+            queryDescriptor.QueryString(
+                q => q.OnFields(f => f.Title, f => f.Content).Query(query));
+            queryDescriptor.Filtered(q => q.Filter(f => f.Term(t => t.FlagCount, 0)));
+
+            var searchDescriptor = new SearchDescriptor<ListingIndexDto>();
+            searchDescriptor.Query(queryDescriptor);
+            
+            var response = _client.Search(searchDescriptor);
+
+            return new SearchResults<ListingIndexDto> {
+                Results = response.Documents.ToList(),
+                TotalResults = response.Total
+            };
+        }
+    }
+}
