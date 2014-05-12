61b4aa36ab9bb94c1afd781bd546564bce3ae663
Gilly Barr <gillyb@gmail.com>
2014-05-12 16:27:56 +0300

    fixing the ListingSearchProvider

---
+++ Classy.Tests/Framework/TestBase.cs
@@ -0,0 +1,35 @@
+﻿using Classy.Interfaces.Search;
+using classy.Manager.Search;
+using Funq;
+
+namespace Classy.Tests.Framework
+{
+    public interface ITestBase
+    {
+        Container Container { get; set; }
+    }
+
+    public class TestBase : ITestBase
+    {
+        public Container Container { get; set; }
+
+        public TestBase()
+        {
+            Container = new Container();
+        }
+    }
+
+    public class SearchProviderTestBase : TestBase
+    {
+        public readonly IListingSearchProvider ListingSearchProvider;
+
+        public SearchProviderTestBase()
+        {
+            Container.Register<ISearchClientFactory>(_ => new SearchClientFactory());
+            Container.Register<IListingSearchProvider>(
+                c => new ListingSearchProvider(c.TryResolve<ISearchClientFactory>()));
+
+            ListingSearchProvider = Container.Resolve<IListingSearchProvider>();
+        }
+    }
+}
---
+++ classifieds/Manager/Search/SearchClientFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Configuration;
+using Classy.Interfaces.Search;
+using Nest;
+
+namespace classy.Manager.Search
+{
+    public class SearchClientFactory : ISearchClientFactory
+    {
+        public static string ElasticConnectionString = ConfigurationManager.AppSettings["SEARCHBOX_URL"];
+
+        public ElasticClient GetClient(string indexName)
+        {
+            var settings = new ConnectionSettings(new Uri(ElasticConnectionString));
+
+            if (indexName != null)
+                settings.SetDefaultIndex(indexName);
+
+            return new ElasticClient(settings);
+        }
+    }
+}
