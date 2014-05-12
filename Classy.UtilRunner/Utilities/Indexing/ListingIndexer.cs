f1390679da602c42d2510144313fd03fefe4c2c3
Gilly Barr <gillyb@gmail.com>
2014-05-12 16:27:40 +0300

    changing the ListingIndexDto a bit

---
+++ Classy.UtilRunner/Utilities/Indexing/ListingIndexer.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using classy.Extentions;
+using Classy.Interfaces.Search;
+using Classy.Models;
+using Classy.Models.Search;
+using Funq;
+using MongoDB.Driver;
+using Nest;
+using ServiceStack.Common;
+
+namespace Classy.UtilRunner.Utilities.Indexing
+{
+    public class ListingIndexer : IUtility
+    {
+        private const int BatchSize = 100;
+        private readonly MongoCollection<Listing> _listings;
+        private readonly ISearchClientFactory _searchClientFactory;
+
+        public ListingIndexer(Container container)
+        {
+            _searchClientFactory = container.Resolve<ISearchClientFactory>();
+            var mongoDatabase = container.Resolve<MongoDatabase>();
+            _listings = mongoDatabase.GetCollection<Listing>("classifieds");
+        }
+        
+        public StatusCode Run(string[] args)
+        {
+            var client = _searchClientFactory.GetClient("listings");
+            client.DeleteIndex<ListingIndexDto>();
+            client.CreateIndex("listings", new IndexSettings());
+            client.MapFromAttributes<ListingIndexDto>();
+
+            client = _searchClientFactory.GetClient("listings");
+
+            var i = 0;
+            var cursor = _listings.FindAll().SetBatchSize(BatchSize);
+            foreach (var listingsBulk in cursor.Bulks(BatchSize))
+            {
+                var toIndex = new List<ListingIndexDto>(BatchSize);
+
+                foreach (var listing in listingsBulk)
+                {
+                    string[] metadata;
+                    if (!listing.Metadata.IsNullOrEmpty())
+                        metadata = listing.Metadata.Select(x => x.Value).ToArray();
+                    else 
+                        metadata = new string[0];
+
+                    toIndex.Add(new ListingIndexDto
+                    {
+                        Id = listing.Id,
+                        AddToCollectionCount = listing.AddToCollectionCount,
+                        BookingCount = listing.BookingCount,
+                        ClickCount = listing.ClickCount,
+                        CommentCount = listing.CommentCount,
+                        Content = listing.Content,
+                        FavoriteCount = listing.FavoriteCount,
+                        FlagCount = listing.FlagCount,
+                        Keywords =
+                            listing.SearchableKeywords != null ? listing.SearchableKeywords.ToArray() : new string[0],
+                        ListingType = listing.ListingType,
+                        Metadata = metadata,
+                        PurchaseCount = listing.PurchaseCount,
+                        Title = listing.Title,
+                        ViewCount = listing.ViewCount
+                    });
+                }
+
+                client.IndexMany(toIndex);
+                
+                Console.WriteLine("Indexed {0} documents", ++i*BatchSize);
+            }
+
+            return StatusCode.Success;
+        }
+    }
+}
--- classifieds/Extentions/EnumerableExtensions.cs
+++ classifieds/Extentions/EnumerableExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace classy.Extentions
@@ -8,6 +9,28 @@ namespace classy.Extentions
         public static IEnumerable<T> EmptyIfNull<T>(this IEnumerable<T> collection)
         {
             return collection ?? Enumerable.Empty<T>();
+        }
+
+        public static IEnumerable<IList<T>> Bulks<T>(this IEnumerable<T> source, int bulkSize)
+        {
+            return Bulks(source, bulkSize, size => size);
+        }
+
+        public static IEnumerable<IList<T>> Bulks<T>(this IEnumerable<T> source, int initialBulkSize, Func<int, int> bulkSizeModifier)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+
+            var bulkSize = initialBulkSize;
+            if (bulkSize <= 0) throw new ArgumentOutOfRangeException("bulkSize", "bulk size must be positive");
+
+            var bulker = new BulkedEnumerator<T>(source);
+
+            // Consume the sequence in bulks until we hit its end
+            while (bulker.TakeNext(bulkSize))
+            {
+                yield return bulker.Current;
+                bulkSize = bulkSizeModifier(bulkSize);
+            }
         }
     }
 }
