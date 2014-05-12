5bfcc1ce50aeed96593f6fdd9ab38337d68cdfa6
Gilly Barr <gillyb@gmail.com>
2014-05-12 16:27:40 +0300

    changing the ListingIndexDto a bit

--- Classy.Interfaces/Search/SearchClientFactory.cs
+++ Classy.Interfaces/Search/SearchClientFactory.cs
@@ -8,19 +8,4 @@ namespace Classy.Interfaces.Search
     {
         ElasticClient GetClient(string indexName);
     }
-
-    public class SearchClientFactory : ISearchClientFactory
-    {
-        public static string ElasticConnectionString = ConfigurationManager.AppSettings["SEARCHBOX_URL"];
-
-        public ElasticClient GetClient(string indexName)
-        {
-            var settings = new ConnectionSettings(new Uri(ElasticConnectionString));
-
-            if (indexName != null)
-                settings.SetDefaultIndex(indexName);
-
-            return new ElasticClient(settings);
-        }
-    }
 }
