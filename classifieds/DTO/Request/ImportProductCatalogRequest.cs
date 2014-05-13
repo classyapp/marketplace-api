6a74f845f281bd1288ad20d505ede51e1d9ecabf
mfractal <mfractal@gmail.com>
2014-05-13 11:34:25 -0300

    starting to work on product import
    starting.

---
+++ classifieds/DTO/Request/ImportProductCatalogRequest.cs
@@ -0,0 +1,26 @@
+﻿using Classy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace classy.DTO.Request
+{
+    public class ImportProductCatalogRequest : BaseRequestDto
+    {
+        public string ProfileId { get; set; }
+        public string AWSFileKey { get; set; }
+        public bool OverwriteListings { get; set; }
+        public bool UpdateImages { get; set; }
+        public int InputFormat { get; set; }
+
+        public ImportProductCatalogRequest(string profileId, string AWSKey, bool overwriteListings, bool updateImages, int inputType)
+        {
+            this.ProfileId = profileId;
+            this.AWSFileKey = AWSKey;
+            this.OverwriteListings = overwriteListings;
+            this.UpdateImages = updateImages;
+            this.InputFormat = inputType;
+        }
+    }
+}
