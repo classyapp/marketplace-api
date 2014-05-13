26c896ac87895df4dcf338a5441fa9a1cea29d4a
gillyb <gillyb@gmail.com>
2014-05-13 19:36:26 +0300

    started organizing the Services into groups

---
+++ classifieds/Services/CommentsService.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using Classy.Auth;
+using classy.Manager;
+using Classy.Models.Request;
+using Classy.Models.Response;
+using ServiceStack.Common.Web;
+using ServiceStack.ServiceInterface;
+
+namespace classy.Services
+{
+    public class CommentsService : Service
+    {
+        public IListingManager ListingManager { get; set; }
+        public IProfileManager ProfileManager { get; set; }
+        public ICollectionManager CollectionManager { get; set; }
+
+        // add comment to post
+        [CustomAuthenticate]
+        public object Post(PostCommentForListing request)
+        {
+            try
+            {
+                var session = SessionAs<CustomUserSession>();
+                CommentView comment = null;
+
+                ListingManager.SecurityContext = session.ToSecurityContext();
+                comment = ListingManager.AddCommentToListing(
+                    request.Environment.AppId,
+                    request.ListingId,
+                    request.Content,
+                    request.FormatAsHtml);
+
+                comment.Profile = ProfileManager.GetProfileById(request.Environment.AppId, comment.ProfileId, null, false, false, false, false, false, false, false, request.Environment.CultureCode);
+
+                return new HttpResult(comment, HttpStatusCode.OK);
+            }
+            catch (KeyNotFoundException kex)
+            {
+                return new HttpError(HttpStatusCode.NotFound, kex.Message);
+            }
+        }
+
+        // add comment to post
+        [CustomAuthenticate]
+        public object Post(PostCommentForCollection request)
+        {
+            try
+            {
+                var session = SessionAs<CustomUserSession>();
+                CommentView comment = null;
+
+                CollectionManager.SecurityContext = session.ToSecurityContext();
+                comment = CollectionManager.AddCommentToCollection(
+                    request.Environment.AppId,
+                    request.CollectionId,
+                    request.Content,
+                    request.FormatAsHtml);
+
+                return new HttpResult(comment, HttpStatusCode.OK);
+            }
+            catch (KeyNotFoundException kex)
+            {
+                return new HttpError(HttpStatusCode.NotFound, kex.Message);
+            }
+        }
+    }
+}
--- classifieds/Services/SearchService.cs
+++ classifieds/Services/SearchService.cs
@@ -1,23 +1,84 @@
-﻿using System.Diagnostics;
+﻿using System.Linq;
 using System.Net;
-using Amazon.AWSSupport.Model;
+using classy.DTO.Request.Search;
 using Classy.Interfaces.Search;
-using Classy.Models;
+using classy.Manager;
+using Classy.Models.Request;
+using Classy.Models.Response;
+using Classy.Models.Response.Search;
 using ServiceStack.Common.Web;
+using ServiceStack.ServiceInterface;
 
 namespace classy.Services
 {
     public class SearchService : Service
     {
+        public IAppManager AppManager { get; set; }
+        public IProfileManager ProfileManager { get; set; }
         public IListingSearchProvider ListingSearchProvider { get; set; }
+        public IListingManager ListingManager { get; set; }
 
-        
-    }
+        public object Post(SearchListingsRequest searchRequest)
+        {
+            var searchResults = ListingSearchProvider.Search(
+                searchRequest.Q, searchRequest.Amount, searchRequest.Page);
 
-    public class SearchListingsRequest : BaseRequestDto
-    {
-        public string Q { get; set; }
-        public int Amount { get; set; }
-        public int Page { get; set; }
+            var listingsFromDb = ListingManager.GetListingsByIds(
+                searchResults.Results.Select(x => x.Id).ToArray(),
+                searchRequest.Environment.AppId,
+                false,
+                searchRequest.Environment.CultureCode);
+
+            var response = new SearchResultsResponse<ListingView>(listingsFromDb, searchResults.TotalResults);
+
+            return new HttpResult(response, HttpStatusCode.OK);
+        }
+
+        public object Get(SearchListings request)
+        {
+            return Post(request);
+        }
+
+        public object Post(SearchListings request)
+        {
+            var listingViews = ListingManager.SearchListings(
+                request.Environment.AppId,
+                request.Tags,
+                request.ListingTypes,
+                request.Metadata,
+                request.PriceMin,
+                request.PriceMax,
+                request.Location ?? request.Environment.GetDefaultLocation(AppManager.GetAppById(request.Environment.AppId).DefaultCountry),
+                request.IncludeComments,
+                request.FormatCommentsAsHtml,
+                request.Page,
+                AppManager.GetAppById(request.Environment.AppId).PageSize,
+                request.Environment.CultureCode);
+
+            return new HttpResult(listingViews, HttpStatusCode.OK);
+        }
+
+        // search profiles
+        public object Get(SearchProfiles request)
+        {
+            return Post(request);
+        }
+
+        public object Post(SearchProfiles request)
+        {
+            var profiles = ProfileManager.SearchProfiles(
+                request.Environment.AppId,
+                request.DisplayName,
+                request.Category,
+                request.Location ?? request.Environment.GetDefaultLocation(AppManager.GetAppById(request.Environment.AppId).DefaultCountry),
+                request.Metadata,
+                request.ProfessionalsOnly,
+                request.IgnoreLocation,
+                request.Page,
+                AppManager.GetAppById(request.Environment.AppId).PageSize,
+                request.Environment.CultureCode);
+
+            return new HttpResult(profiles, HttpStatusCode.OK);
+        }
     }
 }
