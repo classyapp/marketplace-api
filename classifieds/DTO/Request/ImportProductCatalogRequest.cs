950071ac227db107c9273295ea29ff9f4a38a179
mfractal <mfractal@gmail.com>
2014-05-13 14:49:08 -0300

    before merge.

--- classifieds/DTO/Request/ImportProductCatalogRequest.cs
+++ classifieds/DTO/Request/ImportProductCatalogRequest.cs
@@ -4,23 +4,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace classy.DTO.Request
+namespace Classy.Models.Request
 {
-    public class ImportProductCatalogRequest : BaseRequestDto
+     public class ImportProductCatalogRequest
     {
-        public string ProfileId { get; set; }
-        public string AWSFileKey { get; set; }
-        public bool OverwriteListings { get; set; }
-        public bool UpdateImages { get; set; }
-        public int InputFormat { get; set; }
+        public string JobId { get; set; }
 
-        public ImportProductCatalogRequest(string profileId, string AWSKey, bool overwriteListings, bool updateImages, int inputType)
+        public ImportProductCatalogRequest(string jobId)
         {
-            this.ProfileId = profileId;
-            this.AWSFileKey = AWSKey;
-            this.OverwriteListings = overwriteListings;
-            this.UpdateImages = updateImages;
-            this.InputFormat = inputType;
+            this.JobId = jobId;
         }
     }
 }
