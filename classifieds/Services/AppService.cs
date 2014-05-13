df2465b7511486e013647d73fefe1fcd88d63318
gillyb <gillyb@gmail.com>
2014-05-13 19:36:26 +0300

    started organizing the Services into groups

---
+++ classifieds/Services/AppService.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using classy.Manager;
+using Classy.Models.Request;
+using Classy.Models.Response;
+using ServiceStack.Common.Web;
+using ServiceStack.ServiceInterface;
+
+namespace classy.Services
+{
+    public class AppService : Service
+    {
+        public IAppManager AppManager { get; set; }
+        public object Get(GetAppSettings request)
+        {
+            var app = AppManager.GetAppById(request.Environment.AppId);
+            return new HttpResult(app.TranslateTo<AppView>(), HttpStatusCode.OK);
+        }
+    }
+}
