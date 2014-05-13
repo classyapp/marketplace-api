f168193fb30778d5e926cd9a16f27bbc58d83d41
mfractal <mfractal@gmail.com>
2014-05-13 11:34:25 -0300

    starting to work on product import
    starting.

---
+++ classifieds/Operations/ProductCatalogImportOperator.cs
@@ -0,0 +1,29 @@
+﻿using classy.DTO.Request;
+using classy.Manager;
+using Classy.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace classy.Operations
+{
+    public class ProductCatalogImportOperator : IOperator<ImportProductCatalogRequest>
+    {
+        private readonly IStorageRepository _storageRepository;
+        private readonly IListingRepository _listingRepository;
+        private readonly IAppManager _appManager;
+
+        public ProductCatalogImportOperator(IStorageRepository storageRepo, IListingRepository listingRepo, IAppManager appManager)
+        {
+            _storageRepository = storageRepo;
+            _listingRepository = listingRepo;
+            _appManager = appManager;
+        }
+
+        public void PerformOperation(ImportProductCatalogRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
