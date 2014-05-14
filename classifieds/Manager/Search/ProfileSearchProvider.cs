f5800f0cd921c0fc20d28b1a93aee0e5ca806564
Gilly Barr <gillyb@gmail.com>
2014-05-14 12:13:42 +0300

    Added index for Profile

---
+++ classifieds/Manager/Search/ProfileSearchProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Classy.Interfaces.Search;
+using Classy.Models.Search;
+using Nest;
+
+namespace classy.Manager.Search
+{
+    public class ProfileSearchProvider : IProfileSearchProvider
+    {
+        private readonly IElasticClient _client;
+
+        public ProfileSearchProvider(ISearchClientFactory searchClientFactory)
+        {
+            _client = searchClientFactory.GetClient("profiles");
+        }
+
+        public SearchResults<ProfileIndexDto> Search(string query, int amount = 25, int page = 1)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
