9a110aa72969d31bb5d8920cac0c09a8b3171e9f
Michael Bar <barmic@gmail.com>
2014-05-29 15:58:48 -0300

    Job import background job

---
+++ Classy.CatalogImportWorker/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using classy.Extensions;
+using Classy.Repository;
+
+namespace Classy.CatalogImportWorker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // set up container
+            Funq.Container _container = new Funq.Container();
+            _container.WireUp();
+
+            // get Jobs repository
+            IJobRepository _jobRepository = _container.Resolve<IJobRepository>();
+        }
+    }
+}
--- classifieds/Manager/DefaultJobManager.cs
+++ classifieds/Manager/DefaultJobManager.cs
@@ -33,7 +33,7 @@ namespace classy.Manager
         public JobView ScheduleCatalogImport(string appId, string profileId, bool overwriteListings, bool updateImages, byte[] catalog, string contentType, int catalogFormat)
         {
             string fileKey = Guid.NewGuid().ToString();
-            //_storageRepository.SaveFile(fileKey, catalog, contentType);
+            _storageRepository.SaveFile(fileKey, catalog, contentType);
 
             Job job = new Job
             {
@@ -52,7 +52,7 @@ namespace classy.Manager
 
             _jobsRepository.Save(job);
             var queueRequest = new ImportProductCatalogJob(job.Id, appId);
-            //_messageQueueClient.Publish<ImportProductCatalogJob>(queueRequest);
+            _messageQueueClient.Publish<ImportProductCatalogJob>(queueRequest);
 
             return new JobView { JobId = job.Id, Status = job.Status };
         }
