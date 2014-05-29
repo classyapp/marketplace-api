3394320950472ad109f3f3fa2d80a7c9182da064
Michael Bar <barmic@gmail.com>
2014-05-29 17:54:25 -0300

    Removed import job mq publishing

--- classifieds/Manager/DefaultJobManager.cs
+++ classifieds/Manager/DefaultJobManager.cs
@@ -17,17 +17,14 @@ namespace classy.Manager
         private ManagerSecurityContext _context = null;
         private IJobRepository _jobsRepository = null;
         private IStorageRepository _storageRepository = null;
-        private IMessageQueueClient _messageQueueClient = null;
 
         public Env Environment { get; set; }
 
         public DefaultJobManager(IJobRepository jobsRepository,
-            IStorageRepository storageRepository,
-            IMessageQueueClient messageQueueClient)
+            IStorageRepository storageRepository)
         {
             _jobsRepository = jobsRepository;
             _storageRepository = storageRepository;
-            _messageQueueClient = messageQueueClient;
         }
 
         public JobView ScheduleCatalogImport(string appId, string profileId, bool overwriteListings, bool updateImages, byte[] catalog, string contentType, int catalogFormat)
@@ -51,8 +48,6 @@ namespace classy.Manager
             };
 
             _jobsRepository.Save(job);
-            var queueRequest = new ImportProductCatalogJob(job.Id, appId);
-            _messageQueueClient.Publish<ImportProductCatalogJob>(queueRequest);
 
             return new JobView { JobId = job.Id, Status = job.Status };
         }
