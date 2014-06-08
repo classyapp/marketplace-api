063c85eec98dd32aab10fe3513b133931649b02b
gillyb <gillyb@gmail.com>
2014-06-08 10:58:35 +0300

    refactored ImageExtensions a bit

--- classifieds/Extentions/ImageExtensions.cs
+++ classifieds/Extentions/ImageExtensions.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
+﻿using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 using System.IO;
-using System.Linq;
-using System.Web;
 
 namespace classy.Extentions
 {
@@ -15,39 +11,47 @@ namespace classy.Extentions
         {
             byte[] buffer = null;
 
-            Stream memoryStream = new MemoryStream(original);
-            using (Image image = Image.FromStream(memoryStream))
+            using (var memoryStream = new MemoryStream(original))
             {
-                if (maxSize > image.Width) {
-                    memoryStream.Close();
-                    memoryStream.Dispose();
-                    return original;
-                }
-                double scale = (double)image.Width / (double)image.Height;
-                int newWidth = scale > 1 ? maxSize : (int)(maxSize * scale);
-                int newHeight = scale > 1 ? (int)(maxSize / scale) : maxSize;
-
-                using (var newImage = new Bitmap(newWidth, newHeight))
+                using (var image = Image.FromStream(memoryStream))
                 {
-                    using (var graphics = Graphics.FromImage(newImage))
+                    if (maxSize > image.Width)
                     {
-                        graphics.SmoothingMode = SmoothingMode.AntiAlias;
-                        graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
-                        graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
-                        graphics.DrawImage(image, new Rectangle(0, 0, newWidth, newHeight));
-                        Stream reducedImage = new MemoryStream();
-                        newImage.Save(reducedImage, ImageFormat.Jpeg);
-                        reducedImage.Seek(0, SeekOrigin.Begin);
-                        buffer = new byte[reducedImage.Length];
-                        reducedImage.Read(buffer, 0, buffer.Length);
-                        reducedImage.Close();
-                        reducedImage.Dispose();
+                        memoryStream.Close();
+                        return original;
+                    }
+
+                    double scale = (double) image.Width/(double) image.Height;
+                    int newWidth = scale > 1 ? maxSize : (int) (maxSize*scale);
+                    int newHeight = scale > 1 ? (int) (maxSize/scale) : maxSize;
+
+                    using (var newImage = new Bitmap(newWidth, newHeight))
+                    {
+                        using (var graphics = Graphics.FromImage(newImage))
+                        {
+                            graphics.SmoothingMode = SmoothingMode.AntiAlias;
+                            graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                            graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+                            graphics.DrawImage(image, new Rectangle(0, 0, newWidth, newHeight));
+
+                            using (var reducedImage = new MemoryStream())
+                            {
+                                newImage.Save(reducedImage, ImageFormat.Jpeg);
+
+                                // TODO: what is this for ??? (we already saved the image)
+                                reducedImage.Seek(0, SeekOrigin.Begin);
+
+                                buffer = new byte[reducedImage.Length];
+                                reducedImage.Read(buffer, 0, buffer.Length);
+
+                                reducedImage.Close();
+                            }
+                        }
                     }
                 }
-            }
 
-            memoryStream.Close();
-            memoryStream.Dispose();
+                memoryStream.Close();
+            }
 
             return buffer;
         }
