3937b9e687b2acdc7bd79383cc3e7b2b37d03d0f
Gilly Barr <gillyb@gmail.com>
2014-06-10 17:58:55 +0300

    initial commit

---
+++ classifieds/DTO/Request/SearchOrderedListings.cs
@@ -0,0 +1,12 @@
+﻿using Classy.Models;
+
+namespace classy.DTO.Request
+{
+    public class SearchOrderedListings : BaseRequestDto
+    {
+        public string[] ListingTypes { get; set; }
+        public int Page { get; set; }
+        public string Date { get; set; }
+        public int? PageSize { get; set; }
+    }
+}
