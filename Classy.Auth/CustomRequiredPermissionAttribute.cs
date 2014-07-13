f17182f80f86280f61c90f23171f955a94c39eac
gillyb <gillyb@gmail.com>
2014-07-13 18:07:07 +0300

    allow commenting from mobile app

--- Classy.Auth/CustomRequiredPermissionAttribute.cs
+++ Classy.Auth/CustomRequiredPermissionAttribute.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using ServiceStack.ServiceHost;
-using ServiceStack.WebHost.Endpoints.Extensions;
 using ServiceStack.ServiceInterface.Auth;
  
 namespace ServiceStack.ServiceInterface
